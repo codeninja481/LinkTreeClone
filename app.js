0b3c4f4208174252d724cdb9983f1886c216c781
John Hughes <hughes.john.jr@gmail.com>
2023-01-01 13:57:04 -0500

    Changes to js for non-secure sites

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const shareButtons = document.querySelectorAll('.tile-share-button')
 console.log(shareButtons)
 
+const unsecuredCopyToClipboard = (text) => { 
+	const textArea = document.createElement("textarea"); 
+	textArea.value=text; 
+	document.body.appendChild(textArea); 
+	textArea.focus();
+	textArea.select(); 
+	try{
+		document.execCommand('copy')
+	}catch(err){
+		console.error('Unable to copy to clipboard',err)
+	}document.body.removeChild(textArea)};
+
 async function copyText(e) {
 //prevent button going to the site
 	e.preventDefault()
 	const link = this.getAttribute('link')
 	console.log(link)
 	try {
-		await navigator.clipboard.writeText(link)
-		alert("Copied the text: " + link)
+		if (window.isSecureContext && navigator.clipboard) {
+            await navigator.clipboard.writeText(link)
+		    alert("Copied the text: " + link)
+        } 
+        else {
+            unsecuredCopyToClipboard(link)
+            alert("Copied the text: " + link)
+        }
 	} catch (err) {
 		console.error(err)
 	}
